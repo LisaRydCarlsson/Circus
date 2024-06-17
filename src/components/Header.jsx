11d0fb349b8e56f8161950c776ef0bf8fc3a240e
LisaRydCarlsson <lisa@rydcarlsson.se>
2024-06-17 15:03:55 +0200

    Header fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,42 @@
 import "../styling/Header.scss";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { NavLink, Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Header = () => {
 	const location = useLocation();
-	const [shouldAnimate, setShouldAnimate] = useState(false);
-	const [isHome, setIsHome] = useState(location.pathname === "/home");
+	const [shouldAnimate, setShouldAnimate] = useState(
+		location.pathname === "/home"
+	);
 
 	useEffect(() => {
-		if (location.pathname === "/home") {
-			setShouldAnimate(true);
-			setIsHome(true);
-		} else {
-			setShouldAnimate(false);
-			setIsHome(false);
-		}
-	}, [location]);
+		setShouldAnimate(location.pathname === "/home");
+	}, [location.pathname]);
+
+	const variants = useMemo(
+		() => ({
+			hidden: {
+				opacity: 0,
+				y: -100,
+				transition: { duration: 2, ease: "easeOut" },
+			},
+			visible: {
+				opacity: 1,
+				y: 0,
+				transition: { duration: 2, ease: "easeOut" },
+			},
+		}),
+		[]
+	);
 
 	return (
 		<header className="header">
 			<nav className="header__nav">
-				<div
-					className={`header__nav--left ${
-						shouldAnimate ? "animate" : "visible"
-					}`}
+				<motion.div
+					className="header__nav--left"
+					initial={shouldAnimate ? "hidden" : "visible"}
+					animate={shouldAnimate ? "visible" : "visible"}
+					variants={variants}
 				>
 					<NavLink
 						to="/attractions"
@@ -43,17 +56,18 @@ const Header = () => {
 					>
 						SHOWS
 					</NavLink>
-				</div>
+				</motion.div>
 				<div className="header__nav--center">
 					<Link to="/home">
 						<h5>welcome to the</h5>
 						<h4>CircuS</h4>
 					</Link>
 				</div>
-				<div
-					className={`header__nav--right ${
-						shouldAnimate ? "animate" : "visible"
-					}`}
+				<motion.div
+					className="header__nav--right"
+					initial={shouldAnimate ? "hidden" : "visible"}
+					animate={shouldAnimate ? "visible" : "visible"}
+					variants={variants}
 				>
 					<NavLink
 						to="/food-drink"
@@ -73,7 +87,7 @@ const Header = () => {
 					>
 						TICKETS
 					</NavLink>
-				</div>
+				</motion.div>
 			</nav>
 		</header>
 	);
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import "../styling/HomePage.scss";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ReadMore from "../components/ReadMore";
